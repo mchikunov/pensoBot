80db97062cf9db7f97520faf2734da28cae86b55
elendi2003 <elendi2003@hotmail.com>
2018-08-26 21:57:39 +0300

    рабочий пенсионер dao и сервис.

--- src/main/java/service/PensionerService.java
+++ src/main/java/service/PensionerService.java
@@ -4,9 +4,8 @@ import model.Pensioner;
 import org.hibernate.HibernateException;
 
 public interface PensionerService {
-
     Pensioner getPensioner(long id) throws HibernateException;
-
     void addPensioner(Pensioner pensioner) throws HibernateException;
-    public Pensioner getPensionerByPhone(String phoneNumber) throws HibernateException;
+    Pensioner getPensionerByPhone(String phoneNumber) throws HibernateException;
+    void deletePensioner(Pensioner pensioner) throws HibernateException;
 }
